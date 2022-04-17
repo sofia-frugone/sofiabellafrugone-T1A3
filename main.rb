efd4901005229ac30cd3983d4c36c2d0d1f54a9b
sofia_frugone <sofiabellaf2000@gmail.com>
2022-04-17 16:37:08 +1000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative 'plant_schedule'
+
+require 'colorize'
+require 'tty-prompt'
+
+## Welcome message
+puts '-----------------------------------------'
+puts 'Welcome to the Botany Enthusiants Tracker'
+puts '-----------------------------------------'
+
+
+
+
+
+
+
