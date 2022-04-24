627eeea3377d9ee64cd6a5b27d054d55c90be372
sofia_frugone <sofiabellaf2000@gmail.com>
2022-04-24 12:37:15 +1000

    Added frap class, added shot sizes

---
+++ frappucino_class.rb
@@ -0,0 +1,12 @@
+## Frap class (name,steps,price)
+class FrappucinoSelect 
+    def initialize(frappucino, price, steps)
+         @frappucino = coffee
+         @price = price
+         @steps = steps
+    end
+
+    def to_s 
+         "The steps for creating a #{@frappucino} is: #{@steps}. The price is $#{@price}."
+    end
+end
--- main.rb
+++ main.rb
@@ -18,7 +18,7 @@ puts "Welcome barista #{barista_name}. Would you like to start (Y/N)?"
 start_program = gets.chomp 
 
 
-if start_program = "Y"
+if start_program == "Y"
     puts "Would you like to view Coffee or Frappucino beverages?"
 end 
 
@@ -32,17 +32,20 @@ if beverage_select == 'Coffee'
   puts "Mocha (3)"
   puts "Chai (4)"
   puts "Latte (5)"
+end
 
-elsif beverage_select == 'Frappucino'
+coffee_menu_select = gets.chomp.to_i
+
+if beverage_select == 'Frappucino'
   puts "Please select a frappucino from the following options: "
-  puts "Strawberries and Cream(1)"
+  puts "Strawberries and Cream (1)"
   puts "White Mocha (2)"
   puts "Javachip (3)"
   puts "Smores (4)"
   puts "Espresso (5)"
 end
 
-coffee_menu_select = gets.chomp.to_i
+frappucino_menu_select = gets.chomp.to_i
 
 ## Coffee select loop
 loop do 
@@ -63,6 +66,59 @@ loop do
     end
     break
 end
+
+
+##Frap select loop
+loop do 
+    if frappucino_menu_select == 1
+        puts FrappucinoSelect.new 'Strawberries and Cream', 5.94, '1) Airate Milk for 2 seconds. 2) Pour and serve'
+
+    elsif frappucino_menu_select == 2
+        puts FrappucinoSelect.new 'White Mocha', 5.94, '1) Airate Milk for 2 seconds. 2) Pour and serve'
+
+    elsif frappucino_menu_select == 3
+        puts FrappucinoSelect.new 'Javachip', 5.94, '1) Airate Milk for 2 seconds. 2) Pour and serve'
+
+    elsif frappucino_menu_select == 4
+        puts FrappucinoSelect.new 'Smores', 5.94, '1) Airate Milk for 2 seconds. 2) Pour and serve'
+
+    elsif frappucino_menu_select == 5
+        puts FrappucinoSelect.new 'Espresso', 5.94, '1) Airate Milk for 2 seconds. 2) Pour and serve'
+    end
+    break
+end
+
+## Coffee Shots
+puts "Would you like to view how many coffee shots for each size? (Y/N)"
+view_shots = gets.chomp
+
+if view_shots == 'Y'
+    puts "Please select: Tall, Grande or Venti"
+end 
+
+shot_selection = gets.chomp
+
+## Shots printed
+if shot_selection == "Tall"
+    puts tall = {'FlatWhite' => 2, 'Cappucino' => 2, 'Mocha' => 2, 'Chai' => 0, 'Latte' => 2}
+    tall.each { |beverage, shots| 
+        puts "Beverage: #{beverage} Shots: #{shots}" 
+    } 
+
+elsif shot_selection == "Grande"
+    grande = {"Flat-White" => 3, "Cappucino" => 2, "Mocha" => 2, "Chai" => 0, "Latte" => 2}
+        grande.each { |size, shots| 
+            puts "Drnk: #{size} Shots: #{shots}" 
+    } 
+
+elsif shot_selection == "Venti"
+    venti = {"Flat-White" => 4, "Cappucino" => 3, "Mocha" => 3, "Chai" => 0, "Latte" => 3}
+        venti.each { |size, shots| 
+            puts "Drnk: #{size} Shots: #{shots}" 
+    } 
+end
+    
+
 
 
 
