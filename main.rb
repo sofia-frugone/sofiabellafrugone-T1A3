4a46e827ef645b7582871e845dad762d7814546d
sofia_frugone <sofiabellaf2000@gmail.com>
2022-04-29 19:06:32 +1000

    Added some methods and string colours

--- main.rb
+++ main.rb
@@ -4,46 +4,43 @@ require 'tty-box'
 
 
 
-## WELCOME MESSAGE
+## GREET
 File.open("welcome.txt", "r") do |file|
     puts file.read()
 end
 
-## START MENU
-print TTY::Box.frame "What would you like to do?", "Start (1)", "Exit (2)"
-#puts "What would you like to do?"
-#puts "Start (1)"
-#puts "Help (2)"
-#puts "Exit (3)"
-
-start_select = gets.chomp.to_i 
-
-
-## START/EXIT menu selections
-if start_select == 1
-  puts "Welcome to the starbucks program! Please enter your name to get started:"
+## ASKS FOR NAME
+begin
+  print TTY::Box.frame "Welcome to the starbucks program! Please enter your name to get started:"
   barista_name = gets.strip
-end
+  puts "Welcome barista #{barista_name.light_green}."
+rescue StandardError => error ## validates name
+  puts "Name must not be empty"
+  puts error.backtrace
+end 
 
-puts "Welcome barista #{barista_name.light_green}. Would you like to start (Y/N)?"
-start_program = gets.chomp 
 
 
-## SELECT BEVERAGE COFFEE
-if start_program == "Y"
-    puts "Please select a coffee from the following options: "
+
+
+## SELECT COFFEE
+def select_beverage
+    puts "Please select a coffee from the following options: ".light_blue
     puts "Flat-White (1)"
     puts "Cappucino (2)"
     puts "Mocha (3)"
     puts "Chai (4)"
     puts "Latte (5)"
-end
+end 
+
+select_beverage
       
 coffee_menu_select = gets.chomp.to_i
   
     
 
-## COFFEE SELECT LOOP
+## COFFEE LOOP
+
 if coffee_menu_select == 1
      puts CoffeeSelect.new 'Flat-White', '1) Airate Milk for 2 seconds. 2) Add fresh coffee shots and pour milk. 3) Serve.'.light_blue
 
@@ -61,70 +58,83 @@ elsif coffee_menu_select == 5
 end
 
 
-## VIEW COFFEE SHOTS LOOP
-puts "Would you like to view how many coffee shots for each size? (Y/N)"
-view_shots = gets.chomp
-
-if view_shots == 'Y'
-    puts "Please select: Tall (1), Grande (2), or Venti (3)"
+## VIEW SHOTS LOOP
+def view_shots 
+    puts "It's important to know how many coffee shots are required for each drink size!".light_blue
+    puts "Please select:" 
+    puts "Tall (1)" 
+    puts "Grande (2)" 
+    puts "Venti (3)"
 end 
+
+view_shots
 
 shot_selection = gets.chomp.to_i
 
-## PRINT COFFEE SHOTS
+## PRINT SHOTS
 if shot_selection == 1
     tall = {'Flat-White' => 2, 'Cappucino' => 2, 'Mocha' => 2, 'Chai' => 0, 'Latte' => 2}
       tall.each { |beverage, shots|
-        puts "Drink: #{beverage}. Shots: #{shots}" 
+        puts "Drink: #{beverage}"
+        puts "Shots: #{shots}" 
+        puts "------------------"
     } 
 
 elsif shot_selection == 2
     grande = {"Flat-White" => 3, "Cappucino" => 2, "Mocha" => 2, "Chai" => 0, "Latte" => 2}
         grande.each { |beverage, shots| 
-            puts "Drink: #{beverage} Shots: #{shots}" 
+            puts "Drink: #{beverage}" 
+            puts "Shots: #{shots}" 
+            puts "------------------"
     } 
 
 elsif shot_selection == 3
     venti = {"Flat-White" => 4, "Cappucino" => 3, "Mocha" => 3, "Chai" => 0, "Latte" => 3}
         venti.each { |beverage, shots| 
-            puts "Drink: #{beverage} Shots: #{shots}" 
+            puts "Drink: #{beverage}" 
+            puts "Shots: #{shots}" 
+            puts "------------------"
     } 
 end
 
 ## DRINK COST
-puts "Would you like to view the cost of each drink? (Y/N)"
-drink_cost = gets.chomp
-
-if drink_cost == "Y"
-    puts "Please select a size: Tall (1), Grande (2), or Venti (3)"
+def drink_cost 
+  puts "Time to learn the prices of the drinks you've just learnt to make!".light_blue
+  puts "Please select a size...again:"
+  puts "Tall (1)"
+  puts "Grande (2)"
+  puts "Venti (3)"
 end 
+
+drink_cost 
+
 
 drink_size = gets.chomp.to_i
 
-## DRINK COST PRINT
+## PRINT DRINK COST 
 if drink_size == 1
     tall = {'Flat-White' => '4.50', 'Cappucino' => '4.50', 'Mocha' => '5.50', 'Chai' => '4.50', 'Latte' => '4.50'}
       tall.each { |drink, cost| 
-        puts "A tall #{drink} costs $#{cost}" 
+        puts "A tall #{drink} costs $#{cost}".light_blue 
     } 
 
 elsif drink_size == 2
     grande = {"Flat-White" => '5.00', "Cappucino" => '5.00', "Mocha" => '6.00', "Chai" => '5.00', "Latte" => '5.00'}
         grande.each { |drink, cost| 
-            puts "A grande #{drink} costs $#{cost}" 
+            puts "A grande #{drink} costs $#{cost}".light_blue
     } 
 
 elsif drink_size == 3
     venti = {"Flat-White" => '5.50', "Cappucino" => '5.50', "Mocha" => '6.50', "Chai" => '5.50', "Latte" => '5.50'}
         venti.each { |drink, cost| 
-            puts "A venti #{drink} costs $#{cost}" 
+            puts "A venti #{drink} costs $#{cost}".light_blue
     } 
 end
 
 
 ## PROGRAM FINISH
 
-puts "Thank you for using the starbucks coffee program.".green 
+puts "Thank you for using the starbucks coffee program.".light_green 
     
 
 
